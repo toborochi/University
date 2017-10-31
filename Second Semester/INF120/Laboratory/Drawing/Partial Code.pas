856daaed8b04a39a59c91dc0997445e2f20ad3d7
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2017-10-31 10:01:26 -0400

    Update Partial Code.pas

--- Second Semester/INF120/Laboratory/Drawing/Partial Code.pas
+++ Second Semester/INF120/Laboratory/Drawing/Partial Code.pas
@@ -1,8 +1,13 @@
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   canvas.Pen.Width:=20;
+  canvas.Pen.Color:=clblue;
+  canvas.Rectangle(5,10,150,150);
   canvas.MoveTo(100,50);
   canvas.lineto(100,100);
+
+
+
 end;
 
 end.
