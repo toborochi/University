0b093e303eba63f55fd9e0aa7af963624c74c026
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2018-09-02 20:06:36 -0400

    Estructuras UPD

--- IV Semester/INF220/Homework/Listas/File3.cpp
+++ IV Semester/INF220/Homework/Listas/File3.cpp
@@ -14,10 +14,10 @@
 #include <cstdlib>
 
 #include "SMemoria.h"
-//#include "TDAListaVector.h"
+#include "TDAListaVector.h"
 #include "TDAListaMemoria.h"
-//#include "TDAListaPuntero.h"
-
+#include "TDAListaPuntero.h"
+#include "TDAPilaVector.h"
 
 using namespace std;
 
@@ -44,98 +44,9 @@ void MostrarOpciones()
 int main()
 {
 
-    // Declaracion de la Lista (Basada en SMemoria)
-	TDAListaMemoria LM;
+	TDAPilaVector PV;
 
-	int opcion,elemento,cantidad,direccion;
 
-    do
-	{
-		// Refresh de la Consola
-		system("cls");
-
-        // Menu de Opciones
-		MostrarOpciones();
-
-		// Opcion del Menu
-		cin>>opcion;
-
-		switch(opcion)
-		{
-			case 1:
-				// Constructor de la Memoria...
-				LM.m = SMemoria();
-				break;
-			case 2:
-				// Pedir espacio en Memoria...
-				cout<<"Digite la Cantidad de Memoria: ";
-
-				cin>>cantidad;
-
-				LM.m.new_espacio(cantidad);
-				break;
-			case 3:
-                // Liberar espacio en Memoria...
-                cout<<"Digite la Direccion de Memoria: ";
-
-				cin>>direccion;
-
-				LM.m.delete_espacio(direccion);
-				break;
-			case 4:
-                // Creacion de la Lista...
-                LM.crear();
-				break;
-			case 5:
-				// Agregacion de elemento en la cabeza de la lista
-				cout<<"Ingrese el Elemento a Insertar: ";
-
-				cin>> elemento;
-
-				if(LM.vacia())    // Pequena Excepcion
-				{
-					LM.inserta(1,elemento);
-				}else
-				{
-                    LM.inserta(LM.primero(),elemento);
-				}
-				break;
-			case 6:
-				// Agregacion de elemento en la cola de la lista
-				cout<<"Ingrese el Elemento a Insertar: ";
-
-				cin>>elemento;
-                if(LM.vacia())
-				 {
-					 LM.inserta(1,elemento);
-				 }else
-				 {
-					LM.inserta(LM.fin(),elemento);
-				 }
-				break;
-			case 7:
-                // Mostramos la Memoria
-				LM.m.mostrar();
-				getch();
-				break;
-			case 8:
-                 // Mostramos la Lista
-				 if(LM.vacia())
-				 {
-					 cout<< "Lista Vacia"<<endl;
-				 }else
-				 {
-                     LM.imprimir();
-                 }
-				 getch();
-				 break;
-			default :
-				break;
-		}
-
-	}while(opcion!=9);
-
-    cout<< "Gracias por usar el programa..." <<endl;
 
 	getch();
 	return 0;
