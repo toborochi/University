32e22574f06ebbe6e8e657532637a7e090e9d77b
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2018-10-11 08:42:20 -0400

    Add files via upload

--- IV Semester/INF221/217002498.asm
+++ IV Semester/INF221/217002498.asm
@@ -1,71 +1,46 @@
 
 ;%%%%%%%%%%%%%%%%%%%%%%%
-; Autor: Leonardo Henry Anez Vladimirovna
-; Registro: 217002498
+;assemble and link with 
+;nasm -f win32 -o P1.o P1.asm -l P1.lst
+;gcc -o P1.exe P1.o
 ;%%%%%%%%%%%%%%%%%%%%%%%
 ;---------------------------------------
 
-;file: 217002498.asm
+; Problema: Pasar los elementos de a1 a a2 y multiplicarlo por 2
+
+;file: P1.asm
 
 extern _printf, _scanf		
 
 segment .data
 ;aqui se declaran variables inicializadas
-
-a DB 0x75
-b DB 0x90
-
-		
+	
+a1 DB 10,20,30,40,50	
+	
 segment .bss
 ;aqui van las variables declaradas pero sin inicializar
 
-mayor RESB 1
-menor RESB 1
+a2 resb 5
 
 segment .text 
 global _main 					
 _main:
-;---------------------------------------s	
-; COMPARACION DE NUMEROS
-;mov al,[a]
-;mov bl,[b]
-;cmp al,bl
-;jg mayorCS
-;	mov [menor],al
-;jmp fin
-;mayorCS: mov [mayor],al
+;---------------------------------------	
 
-;MULTIPLICACION SIN SIGNO
-
-;mov al,100
-;mov cl,25
-;mul cl
-
-;MULTIPLICACION con SIGNO
-
-;mov al,-1
-;mov cl,-66
-;imul cl
-
-
-;DIVISION
-NOP
-MOV AX,-95
-MOV CL,12
-IDIV CL
-
-; DIVISION 251/12
-NOP
-MOV AX,251
-MOV CL,12
-DIV CL
-
-nop
-mov al,-95
-cbw ;investigar mnemoico, extiende el byte  a word
-mov cl,12
-idiv cl
-
+	mov ecx,5
+	mov ebx,a1
+	mov esi,a2
+ciclo:  
+		xor eax,eax
+		mov al,2
+		imul byte[ebx]
+		
+		mov [esi],eax
+		
+		inc esi
+		inc ebx
+		dec ecx
+		jnz ciclo
 
 ret
 ;---------------------------------------
