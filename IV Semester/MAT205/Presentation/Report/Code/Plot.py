9a5ebd08d8938c2cfdd131721e7fcab46c8d9690
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2018-11-24 11:40:10 -0400

    Metodos Numericos

--- IV Semester/MAT205/Presentation/Report/Code/Plot.py
+++ IV Semester/MAT205/Presentation/Report/Code/Plot.py
@@ -20,3 +20,26 @@ ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
 plt.show()
+
+
+# Valor Inicial
+
+def g(x):
+	return (1.6*x+13)**(1./3)
+
+
+p0 = 2.49
+i  = 0
+MAX = 50
+TOL = 0.0005
+
+while(i<=MAX):
+	p1 = g(p0)
+	p2 = g(p1)
+	p = p0 - (((p1-p0)**2)/(p2-2*p1+p0))
+	print(str(i)+" "+str(p0)+" "+str(p1)+" "+str(p2)+" "+str(p))
+	if (abs(p-p0)<TOL) :
+		break
+	i = i + 1
+	p0 = p
+
