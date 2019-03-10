9ecb13bdc4e82acc0840f922f2c5e00d4278d828
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-03-09 20:25:50 -0400

    Grafica

---
+++ 5th Semester/ELC102/Prueba GP/Prueba GP/Clases/Poligono.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herramientas.Clases
+{
+    class Poligono
+    {
+        private List<Punto> PuntosPoligono = new List<Punto>();
+        
+        public void AddPunto(float x,float y,float z)
+        {
+            PuntosPoligono.Add(new Punto(x,y,z));
+        }
+        
+    }
+}
---
+++ 5th Semester/ELC102/Prueba GP/Prueba GP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Herramientas.Clases;
+
+namespace ProgramacionGrafica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola como estas!");
+
+            Punto Punto = new Punto();
+            Console.WriteLine(Punto.x);
+
+            Console.ReadKey();
+        }
+    }
+}
