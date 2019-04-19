edff3939aa63ca19ab7daa7bdac27be096eb5a89
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-04-19 19:49:18 -0400

    Hola

---
+++ 5th Semester/ELC102/Transformaciones/CG4/CG/Utilidades/SistemaGuardado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CG.Utilidades
+{
+    public static class SistemaGuardado
+    {
+        private static XmlSerializer ObjetoSerializado = new XmlSerializer(typeof(Objeto));
+        
+        public static void GuardarObjeto(Objeto objeto,string lugar)
+        {
+            StreamWriter write = new StreamWriter(lugar);
+            ObjetoSerializado.Serialize(write,objeto);
+        }
+
+        
+        public static Objeto CargarObjeto(string lugar)
+        {
+            StreamReader reader = new StreamReader(lugar);
+            Objeto obj = (Objeto)ObjetoSerializado.Deserialize(reader);
+            if(obj!=null)
+            {
+                return obj;
+            }
+            return new Objeto();
+        }
+    }
+}
