578ccd7f2f68534131d327ab5a78a437b79dbe36
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-04-24 12:10:04 -0400

    Update Recursion.pl

--- 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.pl
+++ 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.pl
@@ -1,17 +1,15 @@
 % 1. mostrarDivisoresDesc(n) : Procedimiento que 
 % muestra los divisores del entero n. Muestra desde el n a 1.
-mostrarDivisoresDesc:- read(N),
-					   divisores(N,1).
+mostrarDivisoresDesc:- read(N),divisores(N,1).
 		   
 divisores(N,I):- I>N,!.
 divisores(N,I):-
-					N mod I =:= 0,
-					I1 is I+1,
-					divisores(N,I1),
-					write(I),nl.
+		N mod I =:= 0,
+		I1 is I+1,
+		divisores(N,I1),
+		write(I),nl.
 divisores(N,I):- I1 is I+1,
-				 divisores(N,I1).
-				 
+		divisores(N,I1).			 
 				
 % 2. mostrarDivisoresComunes(n, m) : Procedimiento que 
 % muestra los divisores comunes de los entero n y m.
