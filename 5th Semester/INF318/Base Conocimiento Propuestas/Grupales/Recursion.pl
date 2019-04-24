4f9b206bc645a4745a4eee7ee1744d48378f3a8a
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-04-24 12:01:26 -0400

    Create Recursion.pl

---
+++ 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.pl
@@ -0,0 +1,11 @@
+mostrarDivisoresDesc:- read(N),
+					   divisores(N,1).
+		   
+divisores(N,I):- I>N,!.
+divisores(N,I):-
+					N mod I =:= 0,
+					I1 is I+1,
+					divisores(N,I1),
+					write(I),nl.
+divisores(N,I):- I1 is I+1,
+				 divisores(N,I1).
