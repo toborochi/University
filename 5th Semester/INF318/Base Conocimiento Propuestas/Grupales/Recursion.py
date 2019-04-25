859f47a79905c143a1455a23e12c185d94487365
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-04-25 08:00:16 -0400

    Update Recursion.py

--- 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.py
+++ 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.py
@@ -1,13 +1,27 @@
-def mostrarDivisoresDesc(n):
+def ite_mostrarDivisoresDesc(n):
 	i = n
 	while(i>=1):
 		if(n%i==0):
 			print(i)
 		i=i-1
+		
+def rec_mostrarDivisoresDesc(n):
+	mostrarDivisoresDesc(n,1)
 
 def mostrarDivisoresDesc(n,i):
 	if(i<=n):
-		f(n,i+1)
+		mostrarDivisoresDesc(n,i+1)
 		if(n%i==0):
 			print(i)
 		
+
+def ite_primo(n):
+	if(n==1):
+		return False
+	i = 2
+	while(i<n):
+		if(n%i==0):
+			return False
+		i = i + 1
+	return True
+	
