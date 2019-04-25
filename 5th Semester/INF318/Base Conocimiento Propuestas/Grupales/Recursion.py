7d39daa8cf693facfab00f75f15a82eeb6002c47
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-04-25 08:32:10 -0400

    Update Recursion.py

--- 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.py
+++ 5th Semester/INF318/Base Conocimiento Propuestas/Grupales/Recursion.py
@@ -1,3 +1,4 @@
+# Mostrar Divisores Descendiente (Iterativo)
 def ite_mostrarDivisoresDesc(n):
 	i = n
 	while(i>=1):
@@ -5,6 +6,7 @@ def ite_mostrarDivisoresDesc(n):
 			print(i)
 		i=i-1
 		
+# Mostrar Divisores Descendiente (Recursivo)
 def rec_mostrarDivisoresDesc(n):
 	mostrarDivisoresDesc(n,1)
 
@@ -13,8 +15,8 @@ def mostrarDivisoresDesc(n,i):
 		mostrarDivisoresDesc(n,i+1)
 		if(n%i==0):
 			print(i)
-		
-
+			
+# Es primo(Iterativo)
 def ite_primo(n):
 	if(n==1):
 		return False
@@ -24,13 +26,25 @@ def ite_primo(n):
 			return False
 		i = i + 1
 	return True
-	
-def rec_primo(n):
-	primo(n,2)
 
-def primo(n,i):
-	if(i>n/2):
-		return True
-	if(n%i==0):
-		return False
-	return primo(n,i+1)
+# Mostrar Factorial hasta N (Iterativo)
+def ite_mostrarFactoriales(n):
+	i = 1
+	f = 1
+	while(i<=n):
+		f = f * i
+		print(str(i)+"!="+str(f))
+		i = i + 1
+		
+# Mostrar Fibonacci hasta el n-esimo termino
+def ite_mostrarFib(n):
+	i = 1
+	a = 0
+	b = 1
+	print(1)
+	while(i<n):
+		f = a + b
+		print(f)
+		a = b
+		b = f
+		i = i + 1
