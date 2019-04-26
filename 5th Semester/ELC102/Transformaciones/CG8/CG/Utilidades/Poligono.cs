0ae288ad680e65970ca9634c10750a79ca7c3432
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-04-25 21:59:14 -0400

    Actualizacion en la edicion de poligonos de un objeto
    Espero sirva, faltan pocas clases ya para el proyecto final 2D.

---
+++ 5th Semester/ELC102/Transformaciones/CG8/CG/Utilidades/Poligono.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG.Utilidades
+{
+    [Serializable]
+    public class Poligono
+    {
+        public List<Punto> Vertices;
+        public string nombre="Poligono";
+        public Transformacion Transformacion;
+
+        public Poligono()
+        {
+            Transformacion = new Transformacion();
+            Vertices = new List<Punto>();
+        }
+
+        public Punto PrimerPunto()
+        {
+            return Vertices[0];
+        }
+
+        public Punto UltimoPunto()
+        {
+            return Vertices[Vertices.Count - 1];
+        }
+
+        public void AnadirPunto(Punto x)
+        {
+            Vertices.Add(x);
+        }
+    }
+}
