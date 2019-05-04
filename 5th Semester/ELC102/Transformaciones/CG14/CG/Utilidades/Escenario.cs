459d3adb6ddcb9881dae185fa99cea84e6d4097a
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-05-04 07:49:17 -0400

    Adding more Features.

---
+++ 5th Semester/ELC102/Transformaciones/CG14/CG/Utilidades/Escenario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG.Utilidades
+{
+    public class Escenario
+    {
+        public List<Objeto> Objetos;
+        public Escenario()
+        {
+            Objetos = new List<Objeto>();
+        }
+    }
+}
