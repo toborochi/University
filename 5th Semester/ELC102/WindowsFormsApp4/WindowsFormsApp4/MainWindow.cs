aadd8afad4313f05fd9049b299df43dbbeb9b1eb
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-05-14 22:22:00 -0400

    Starting with OpenTK

---
+++ 5th Semester/ELC102/WindowsFormsApp4/WindowsFormsApp4/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+namespace techdump.opengl.Components
+{
+    public sealed class MainWindow : GameWindow
+    {
+
+        public MainWindow()
+        : base(640, // initial width
+        480, // initial height
+        GraphicsMode.Default,
+        "dreamstatecoding",  // initial title
+        GameWindowFlags.Default,
+        DisplayDevice.Default,
+        3, // OpenGL major version
+        0, // OpenGL minor version
+        GraphicsContextFlags.ForwardCompatible
+        )
+        {
+            base.Location = new System.Drawing.Point(20, 20);
+            Title += ": OpenGL Version: " + GL.GetString(StringName.Version);
+        }
+    }
+}
---
+++ 5th Semester/ELC102/WindowsFormsApp4/WindowsFormsApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using techdump.opengl.Components;
+
+namespace WindowsFormsApp4
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+
+            new MainWindow().Run(30);
+        }
+    }
+}
