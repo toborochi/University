aab874a3bee9df69cb8247eb6f011050b826774a
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-05-15 19:46:01 -0400

    Create PROM.asm

---
+++ PROM.asm
@@ -0,0 +1,43 @@
+extern _printf, _scanf, _gets	
+
+segment .data
+;Variables Inicializadas
+
+arreglo dd 22,44,55,100,33,17
+n	    dd 6
+
+segment .bss
+;Variables sin Inicializar
+
+promedio resd 1
+
+segment .text 
+	
+global _main 					
+_main:
+;----------------------------------------------------------------------------	
+
+xor ecx,ecx
+xor ebx,ebx
+
+mov cx,[n]
+ciclo:
+
+	mov eax,[arreglo+ebx*4]
+	imul dword[arreglo+ebx*4]
+	imul dword[arreglo+ebx*4]
+	
+	add [promedio],eax
+	
+	inc ebx
+	loop ciclo
+	
+	mov eax,[promedio]
+	cdq
+	idiv dword[n]
+	mov [promedio],eax
+	
+
+ret
+;----------------------------------------------------------------------------
+;...............................subrutina....................................	
