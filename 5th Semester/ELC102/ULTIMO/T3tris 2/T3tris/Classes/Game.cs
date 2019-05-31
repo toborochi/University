dbed2352e539ad90a50dd75546d6c30b9f020e4f
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2019-05-31 16:58:37 -0400

    Tetris3D Update for Computer Graphics class
    Working on Tetronimos. Next thing to do: Game Logic

---
+++ 5th Semester/ELC102/ULTIMO/T3tris 2/T3tris/Classes/Game.cs
@@ -0,0 +1,206 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
+using OpenTK.Audio;
+using System.IO;
+using System.Media;
+
+namespace T3tris.Classes
+{
+    public class Game : GameWindow
+    {
+        #region Variables de Juego
+        Container container;
+        Camera camera;
+        #endregion
+
+        #region Constructor
+        public Game()
+            :base(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT, new GraphicsMode(32, 24, 8, 4),Settings.APP_NAME)
+        {
+            Location = Settings.InitialLocation();
+        }
+        #endregion
+
+        Tetromino J = new Tetromino();
+        float xpos=0, ypos = 0,zpos=15;
+        float xrot = 0, yrot = 0, zrot = 0;
+        float deltaTime = 0;
+        float altura = 15,posicionX=0,posicionZ=0;
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            J.TipoPieza = Tetromino.Tipo.J;
+
+            // Contenedor
+            container = new Container(10, 10, 16);
+
+            // Camara
+            camera = new Camera(new Vector3(0, 0, 0), (float)Math.PI / 4, (float)Width / (float)Height, 1f, 100f);
+
+            // A donde esta viendo la camara
+            camera.Target = new Vector3(((float)container.length) / 2.0f, ((float)container.depth) / 2.0f, ((float)container.height) / 2.0f);
+            
+            GL.Enable(EnableCap.DepthTest);
+            GL.Viewport(0, 0,Width, Height);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref camera.ViewCamera);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+        }
+        
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            deltaTime += (float)this.UpdatePeriod;
+
+            if (deltaTime > Settings.INTERVAL)
+            {
+                altura--;
+                deltaTime = 0;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.Right))
+            {
+                xpos += 5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.Left))
+            {
+                xpos -= 5f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.Up))
+            {
+                ypos += 5f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.Down))
+            {
+                ypos -= 5f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.ShiftLeft))
+            {
+                zpos -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.ControlLeft))
+            {
+                zpos += 1f;
+            }
+
+            if (Math.Abs(zpos) <30) { zpos = 30; }
+            if (Math.Abs(zpos) >70) { zpos = 70; }
+
+            if ((float)Math.Abs(ypos)>89)
+            {
+                ypos = 89 * Math.Sign(ypos);
+            }
+
+            camera.distance = zpos;
+
+           
+
+            if (Keyboard.GetState().IsKeyDown(Key.Escape))
+            {
+               
+                Exit();
+            }
+
+
+            J.transform.Position = new Vector3(posicionX, altura, posicionZ);
+            J.transform.Rotation = new Vector3(xrot, yrot, zrot);
+
+        }
+
+
+        float ClampAngle(float x)
+        {
+            x = x % 360;
+            if (x < 0)
+                x += 360;
+            return x;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0,Width, Height);
+            camera.OnWindowResize((float)Math.PI / 4, (float)Width / (float)Height, 1f, 100f);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref camera.ViewCamera);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if (Keyboard.GetState().IsKeyDown(Key.Z))
+            {
+                xrot += 90;
+               
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.X))
+            {
+                yrot += 90;
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.C))
+            {
+                zrot += 90;
+               
+            }
+
+
+            if (Keyboard.GetState().IsKeyDown(Key.A))
+            {
+                posicionX--;
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.D))
+            {
+                posicionX++;
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.W))
+            {
+                posicionZ--;
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.S))
+            {
+                posicionZ++;
+
+            }
+
+            zrot %= 360;
+            xrot %= 360;
+            yrot %= 360;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.LoadIdentity();
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref camera.ViewCamera);
+            
+            // Actualizar Camara
+            camera.updateCameraPosition(MathHelper.DegreesToRadians(-ypos), MathHelper.DegreesToRadians(-xpos), zpos*7.5f);
+   
+            // Dibujar Container
+            container.drawContainer();
+           
+            // Dibujar Pieza Actual
+            J.Draw();
+
+            SwapBuffers();
+        }
+      
+    }
+}
---
+++ 5th Semester/ELC102/ULTIMO/T3tris 2/T3tris/Classes/Tetronimo.cs
@@ -0,0 +1,147 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3tris.Classes
+{
+    class Tetromino : Piece
+    {
+        public enum Tipo {
+            I,J,L,Z,S,O
+        }
+        public Tipo TipoPieza = Tipo.I;
+
+        public override void Draw()
+        {
+            
+            GL.PushMatrix();
+            GL.Translate(transform.Position);
+            GL.Rotate(transform.Rotation.X, new Vector3(1, 0, 0));
+            GL.Rotate(transform.Rotation.Y, new Vector3(0, 1, 0));
+            GL.Rotate(transform.Rotation.Z, new Vector3(0, 0, 1));
+            switch (TipoPieza)
+            {
+                case Tipo.I:  I(); break;
+                case Tipo.J:  J(); break;
+                case Tipo.L:  L(); break;
+            }
+            GL.PopMatrix();
+        }
+
+        private void I()
+        {
+
+            GL.PushMatrix();
+            Primitive.cube(0, 0, 0);
+            Primitive.cube(1, 0, 0);
+            Primitive.cube(2, 0, 0);
+            Primitive.cube(3, 0, 0);
+            GL.PopMatrix();
+        }
+
+        private void J()
+        {
+
+            GL.PushMatrix();
+            Primitive.cube(0, 0, 1);
+            Primitive.cube(1, 0, 1);
+            Primitive.cube(2, 0, 1);
+            Primitive.cube(2, 0, 0);
+            GL.PopMatrix();
+        }
+
+        private void L()
+        {
+
+            GL.PushMatrix();
+            Primitive.cube(0, 0, 0);
+            Primitive.cube(0, 0, 1);
+            Primitive.cube(1, 0, 1);
+            Primitive.cube(2, 0, 1);
+            GL.PopMatrix();
+        }
+    }
+    
+
+    
+    /*
+    class PiezaL : IPieza
+    {
+        private void L()
+        {
+
+            GL.PushMatrix();
+            GL.Translate(x, y, z);
+            cubo.draw(0, 0, 1);
+            cubo.draw(1, 0, 1);
+            cubo.draw(2, 0, 1);
+            cubo.draw(2, 0, 0);
+            GL.PopMatrix();
+        }
+    }
+
+    class PiezaO : IPieza
+    {
+        public void Draw(float x, float y, float z)
+        {
+
+            GL.PushMatrix();
+            GL.Translate(x, y, z);
+            cubo.draw(0, 0, 0);
+            cubo.draw(1, 0, 0);
+            cubo.draw(0, 0, 1);
+            cubo.draw(1, 0, 1);
+            GL.PopMatrix();
+        }
+    }
+
+    class PiezaS : IPieza
+    {
+        public void Draw(float x, float y, float z)
+        {
+
+            GL.PushMatrix();
+            GL.Translate(x, y, z);
+            cubo.draw(0, 0, 1);
+            cubo.draw(1, 0, 1);
+            cubo.draw(1, 0, 0);
+            cubo.draw(2, 0, 0);
+            GL.PopMatrix();
+        }
+    }
+
+    class PiezaT : IPieza
+    {
+        public void Draw(float x, float y, float z)
+        {
+
+            GL.PushMatrix();
+            GL.Translate(x, y, z);
+            cubo.draw(0, 0, 1);
+            cubo.draw(1, 0, 1);
+            cubo.draw(2, 0, 1);
+            cubo.draw(1, 0, 0);
+            GL.PopMatrix();
+        }
+    }
+
+    class PiezaZ : IPieza
+    {
+        public void Draw(float x, float y, float z)
+        {
+
+            GL.PushMatrix();
+            GL.Translate(x, y, z);
+            cubo.draw(0, 0, 0);
+            cubo.draw(1, 0, 0);
+            cubo.draw(1, 0, 1);
+            cubo.draw(2, 0, 1);
+            GL.PopMatrix();
+        }
+    }*/
+
+}
